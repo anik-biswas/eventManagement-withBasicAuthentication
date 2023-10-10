e368c77aaa491adcf4de8ab7ad04987b1ea5d86b
anik-biswas <anik.biswas.an@gmail.com>
2023-10-09 21:49:52 -0700

    final update with Error page

--- src/component/errorpage/ErrorPage.jsx
+++ src/component/errorpage/ErrorPage.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>
-            this is error
+        <div className='h-screen justify-center items-center text-center'>
+        <div className='text-xl font-bold '>
+            <h2 className='my-5'>OPPS! </h2>
+            <h2 className="text-6xl text-red-500">404</h2>
+           <Link to="/"><button className='btn my-5'>Home</button></Link> 
+        </div>
         </div>
     );
 };
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
     </div>
   </nav> 
   <aside>
-    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © 2023 - All right reserved by Event Management</p>
   </aside>
 </footer>  
         </div>
